e9ce8d7bb0bf319d4b923a2a2738e420c10066cd
Jose Camargo <79095172+Jose2004-Cr@users.noreply.github.com>
2024-09-10 00:45:11 -0500

    modificando dasbhorar

--- resources/views/components/sider.blade.php
+++ resources/views/components/sider.blade.php
@@ -50,9 +50,10 @@
                         <a href="{{ route('tiket') }}" onclick="tiket()"
                             class="flex items-center p-2 text-white rounded-lg hover:bg-[#bbbaba] group">
                             <div class="flex-shrink-0 w-6 h-6">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
-                                </svg>
+                            <svg class="h-8 w-8 text-gray-900"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"/>
+</svg>
+
                             </div>
                             <span class="flex-1 transition-opacity duration-300 ease-in-out transform opacity-0 ms-3 whitespace-nowrap">REPORTE</span>
                         </a>
