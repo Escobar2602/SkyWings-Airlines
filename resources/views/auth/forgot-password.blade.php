de5314af56f60d0057362aaea74e0a405adaeb41
Carlos Escobar <carloslastre2602@gmail.com>
2024-09-10 16:01:10 -0500

    actual

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,29 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="flex items-center justify-center min-h-screen bg-slate-100">
+        <div class="container max-w-md bg-white p-6 rounded-lg shadow-md">
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('¿Olvidaste tu contraseña? No hay problema. Solo indícanos tu dirección de correo electrónico y te enviaremos un enlace para restablecer tu contraseña que te permitirá elegir una nueva.') }}
+            </div>
+
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button>
+                        {{ __('Email Password Reset Link') }}
+                    </x-primary-button>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,73 @@
 <x-guest-layout>
-    <div class="flex items-center justify-center min-h-screen bg-center bg-cover"
+    <div class="flex items-center justify-center min-h-screen bg-center bg-cover relative"
         style="background-image: url('{{ asset('images/983433.png') }}');">
+        <!-- Overlay -->
+        <div class="absolute inset-0 bg-black/50"></div>
+
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <div class="bg-white/30 p-8 rounded-lg shadow-md w-full max-w-xl filter backdrop-blur-lg">
+        <div class="relative z-10 bg-white/30 p-8 rounded-lg shadow-md w-full max-w-xl filter backdrop-blur-xl text-center">
             <!-- Logo -->
             <div class="flex justify-center mb-6">
-                <img src="{{ asset('gif/the-plane-13509_512.gif') }}" alt="Descripción del GIF" class="w-48 flex h-auto">
+                <img src="{{ asset('gif/the-plane-13509_512.gif') }}" alt="Descripción del GIF" class="w-48 h-auto animate-pulse">
             </div>
 
             <!-- Título -->
-            <h2 class="text-2xl font-semibold text-center text-indigo-900 mb-2">Ingresa tu usuario</h2>
-            <p class="text-center text-gray-500 mb-6">Si ya eres parte de AEROL-NEA, ingresa tus datos:</p>
+            <h2 class="text-3xl font-semibold text-center text-indigo-900 mb-4">Ingresa tu usuario</h2>
+            <p class="text-center text-gray-300 mb-6">Si ya eres parte de Anemis, ingresa tus datos:</p>
 
             <!-- Formulario -->
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('login') }}" class="space-y-4">
                 @csrf
 
                 <!-- Email Address -->
-                <div class="mb-4">
-                    <x-input-label for="email" :value="__('Email o Número de socio')" />
+                <div class="mb-4 text-center">
+                    <x-input-label for="email" :value="__('Email')" class="text-left ml-16"/>
                     <x-text-input id="email"
-                        class="block mt-1 w-full px-4 py-2 border border-red-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400"
-                        type="text" name="email" :value="old('email')" placeholder="Ingresa un email o Número de socio"
+                        class="block mx-auto mt-1 w-3/4 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400 transition duration-300"
+                        type="text" name="email" :value="old('email')" placeholder="Ingresa un email"
                         required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
                 <!-- Password -->
-                <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
+                <div class="mt-4 text-center">
+                    <x-input-label for="password" :value="__('Password')" class="text-left ml-16"/>
                     <x-text-input id="password"
-                        class="block mt-1 w-full px-4 py-2 border rounded-lg focus:outline-none" type="password"
-                        name="password" required autocomplete="current-password" />
+                        class="block mx-auto mt-1 w-3/4 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400 transition duration-300"
+                        type="password" name="password" required autocomplete="current-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
                 <!-- Remember Me -->
-                <div class="block mt-4">
+                <div class="block mt-4 text-center">
                     <label for="remember_me" class="inline-flex items-center">
                         <input id="remember_me" type="checkbox"
-                            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 transition duration-300"
                             name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <span class="ml-2 text-sm text-gray-300">{{ __('Remember me') }}</span>
                     </label>
                 </div>
 
                 <!-- Botón Continuar -->
-                <div class="flex items-center justify-center mt-4">
+                <div class="flex items-center justify-center mt-6">
                     <button
-                        class="w-full bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
+                        class="w-3/4 bg-gradient-to-r from-red-500 to-red-600 hover:from-red-600 hover:to-red-700 text-white font-bold py-2 px-4 rounded-lg transition-transform duration-300 transform hover:scale-105">
                         {{ __('Continuar') }}
                     </button>
                 </div>
             </form>
 
             <!-- Crear cuenta -->
-            <div class="text-center mt-4">
-                <a href="{{ route('register') }}" class="text-pink-600 font-semibold hover:underline">Crear cuenta</a>
+            <div class="text-center mt-6">
+                <a href="{{ route('register') }}" class="text-pink-400 font-semibold hover:underline hover:text-pink-500 transition duration-300">Crear cuenta</a>
             </div>
 
             <!-- Recuperar acceso -->
             <div class="text-center mt-4">
                 @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}" class="text-blue-600 hover:underline">Recupera el
-                        acceso</a>
+                    <a href="{{ route('password.request') }}" class="text-blue-400 hover:underline hover:text-blue-500 transition duration-300">Recupera el acceso</a>
                 @endif
             </div>
         </div>
